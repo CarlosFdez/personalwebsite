13bb4ee076347a33a936b1235a578ceda3a9039f
Carlos Fernandez <CarlosFdez@users.noreply.github.com>
2017-11-08 15:14:17 -0500

    Fixed favicon loading

--- server/index.ts
+++ server/index.ts
@@ -12,6 +12,7 @@ import { ApiClient } from '../lib/apiclient';
 // simple configuration data.
 // todo: flesh out. For example, we may want nginx to serve assets in production
 const port = 5000;
+const static_location = path.join(__dirname, '../assets/static');
 const assets_location = path.join(__dirname, '../assets/build');
 const portfolio_location = path.join(__dirname, '../portfolio_data/', 'test/');
 const apiRoot = `http://127.0.0.1:${port}`;
@@ -20,6 +21,9 @@ const app = express();
 const portfolio = getPortfolio(portfolio_location);
 const apiClient = new ApiClient(apiRoot);
 
+app.use('/assets/static', express.static(static_location));
+app.use('/assets', express.static(assets_location));
+
 // Use nunjucks as our server-side render engine
 // We don't use it for very much, React does most of the work
 nunjucks.configure(path.join(__dirname, 'templates'), {
@@ -27,7 +31,6 @@ nunjucks.configure(path.join(__dirname, 'templates'), {
     express: app
 });
 
-app.use('/assets', express.static(assets_location));
 
 // register the routes listed in the routes file
 registerApiRoutes(app, portfolio);
