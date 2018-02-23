7439aa216e73e36d2a3d9a1616cd8bc7952166e1
Carlos Fernandez <cfern1990@gmail.com>
2018-02-23 18:40:04 -0500

    Fix connection code (updated for driver version 3)

--- server/lib/db.ts
+++ server/lib/db.ts
@@ -2,23 +2,16 @@
 
 import { MongoClient, Db, MongoClientOptions } from 'mongodb';
 
-const url = 'mongodb://localhost:27017/personalwebsite';
+const dbName = 'personalwebsite'
+const url = `mongodb://localhost:27017/`;
 
-// each subscription to the same promise creates a new connection (Db object)
-let connectionPool : Promise<void | Db>;
+// Store the single existing mongo client. From the client we get new connections
+let client : MongoClient;
 
 let options : Partial<MongoClientOptions> = {
     connectTimeoutMS: 5000,
     reconnectTries: 4
-}
-
-/** Internal function to create the connection pool */
-function createConnectionPool() {
-    connectionPool = MongoClient.connect(url, options).catch(err => console.error(err));
-}
-
-// initialize the connection pool
-createConnectionPool();
+};
 
 /**
  * Asynchronously connects to a mongo db instance located at
@@ -26,32 +19,33 @@ createConnectionPool();
  * @param retries Number of times to retry. Negative avoids reconstructing the connection pool.
  */
 export async function connect(retries=0) : Promise<Db> {
-    let connection: void | Db = await connectionPool;
-
-    // Note: isConnected() is an internal method, but mongo does not provide
-    // us any other way to determine if the connection is healthy 
-    // If we don't do this, then mongo will always throw "Topology was destroyed"
-    if (!connection || !(connection as any).serverConfig.isConnected()) {
-        // Retry a connection
-        if (retries >= 0) {
-            createConnectionPool();
-            return await connect(retries-1);
+    if (!client || !client.isConnected(dbName)) {
+        client = null;
+        
+        if (retries < 0) {
+            // If we get here, its a failed result.
+            throw new Error("connection closed");
         }
 
-        // If we get here, its a failed result.
-        throw new Error("connection closed");
+        // attempt connecting
+        let result = await MongoClient.connect(url, options).catch(err => console.error(err));
+        if (result) {
+            client = result;
+        } else {
+            return await connect(retries-1);
+        }
     }
-    return connection;
+    
+    return client.db(dbName);
 }
 
 // internally used to close all connections when the program quits
 function close() {
-    connect().then(async (db) => {
-        db.close();
-    });
+    if (client) {
+        client.close();
+    }
 }
 
 // idea came from https://github.com/alaingalvan/alain.xyz/blob/53a29be04ffa66d0be4513cc9f6a427d4f769910/packages/backend/src/db.ts
-process
-    .on('SIGTERM', close)
-    .on('SIGINT', close);
+process.on('SIGTERM', close)
+process.on('SIGINT', close);
