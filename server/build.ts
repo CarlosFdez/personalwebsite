59ad9db38ee417a993bb8013cd482d4a73e80a34
Carlos Fernandez <cfern1990@gmail.com>
2018-06-12 21:48:38 -0400

    Improved error messages on invalid portfolio build args

--- server/build.ts
+++ server/build.ts
@@ -25,9 +25,21 @@ async function directoryExists(pathTest : string) {
 }
 
 let builder = (async function() {
+    let dataRoot = path.join(__dirname, '../portfolio_data/');
+    if (!(await directoryExists(dataRoot))) {
+        console.error("ERROR: Missing portfolio_data/ folder");
+        process.exit(0);
+    }
+
+    // valid environments are the folders inside of the data root
+    let validEnvironments = (await fs.readdir(dataRoot))
+        .filter(f => fs.statSync(path.join(dataRoot, f)).isDirectory());
+    let validEnvironmentStr = `Valid environments are ${validEnvironments.join(', ')}.`;
+    
     let directory = process.argv[2];
     if (!directory) {
-        console.error("Error: Portfolio environment required");
+        let environmentStr = validEnvironments.join(', ');
+        console.error("Error: Portfolio environment required. " + validEnvironmentStr);
         process.exit(0);
     }
     if (!directory.match(/[a-zA-Z0-9]+/)) {
@@ -35,10 +47,11 @@ let builder = (async function() {
         process.exit(0);
     }
 
-    let rootPath = path.join(__dirname, '../portfolio_data/', directory);
+    let rootPath = path.join(dataRoot, directory);
     let exists = await directoryExists(rootPath);
     if (!exists) {
-        console.error(`Error: Portfolio directory ${directory} doesn't exist in portfolio_data`);
+        console.error(`Error: Portfolio directory ${directory} doesn't exist ` +
+            `in portfolio_data. ${validEnvironmentStr}`);
         process.exit(0);
     }
     
